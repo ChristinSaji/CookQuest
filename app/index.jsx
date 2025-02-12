807782016e3549f65e44dfa4624b8958475cf901
Christin Saji <christinsaji01@gmail.com>
2025-02-12 15:48:20 -0400

    add NativeWind setup

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,14 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-gray-100">
+      <View className="p-4 bg-blue-500 rounded-lg">
+        <Text className="text-white text-lg font-bold">
+          Welcome to CookQuest
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
